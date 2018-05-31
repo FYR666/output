2c3ef7209ac908b9dc2a07da6e1dd45072c534c5
FYR666 <31131580+FYR666@users.noreply.github.com>
2018-05-31 21:14:30 +0800

    third

--- output/main.cpp
+++ output/main.cpp
@@ -1,12 +1,12 @@
 #include"stdio.h"
 void main()
 {
-	int i, j,k;
+	int i,j,k;
 	for (i = 0; i < 6; i++)
 	{
 		for (k = 6; k >= i; k--)
 			printf(" ");
-		for (j = 0; j < i; j++)
+		for (j = 0; j < 2*i-1; j++)
 			printf("*");
 		printf("\n");
 	}
