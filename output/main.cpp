7eeb9b316beb9d037b7d347699beff1b8e3c1dc4
FYR666 <31131580+FYR666@users.noreply.github.com>
2018-05-31 21:18:46 +0800

    fourth commit

--- output/main.cpp
+++ output/main.cpp
@@ -1,12 +1,20 @@
 #include"stdio.h"
 void main()
 {
-	int i,j,k;
+	int i, j, k;
 	for (i = 0; i < 6; i++)
 	{
 		for (k = 6; k >= i; k--)
 			printf(" ");
 		for (j = 0; j < 2*i-1; j++)
+			printf("*");
+		printf("\n");
+	}
+	for (i = 6; i >= 0; i--)
+	{
+		for (k = 6 - i; k >= 0; k--)
+			printf(" ");
+		for (j = 2 * i - 1; j > 0; j--)
 			printf("*");
 		printf("\n");
 	}
